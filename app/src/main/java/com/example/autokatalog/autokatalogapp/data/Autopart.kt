2feb6f4787a1d60a7c169c40e451337398a86278
Asus <andryevtihiev@gmail.com>
2025-02-24 21:24:21 +0300

    first commit

---
+++ app/src/main/java/com/example/autokatalog/autokatalogapp/data/Autopart.kt
@@ -0,0 +1,15 @@
+package com.example.autokatalog.autokatalogapp.data
+
+data class Autopart(
+    val tipe:String = "",
+    val subTipe:String = "",
+    val name:String = "",
+    val code:String = "",
+    val CarTipe:String = "",
+    val CarSeries:String = "",
+    val CarModel:String = "",
+    val CarModification:String = "",
+    val Creator:String = "",
+    val NumInWarehouse:String = "",
+    val Cost:String = ""
+)
